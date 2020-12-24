4c4fd3a073ccf329d7fa100bae3d20e972cb992d
tacstick <pootisman1131@gmail.com>
2020-12-24 01:03:43 -0300

    added cs

---
+++ cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("escribi lo que quieras y te lo repito, puto");
+            input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
